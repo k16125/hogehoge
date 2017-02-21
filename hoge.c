4f3bafc99b6f79b0223e29b8b0daf0cba78b609e
k16125kk <so05dnoakaunnto@gmail.com>
2017-02-21 14:54:41 +0900

    hoge

---
+++ hoge.c
@@ -0,0 +1,22 @@
+#include<stdio.h>
+#include<stdlib.h>
+
+char *makememo(){
+  char *p;
+  if((p = (char *)malloc(10000*sizeof(char)))== NULL){
+    printf("out of memory\n");
+    exit(EXIT_FAILURE);
+  }
+  return p;
+}
+
+int main (void){
+  //  int *i;
+  char *c;
+  printf("%d\n",*c);
+  c = makememo();  
+  //scanf("%s",c+10000);
+  while(scanf("%s",c)!=EOF){}
+  printf("%s   \n",c+10000);
+  return 0;
+}
