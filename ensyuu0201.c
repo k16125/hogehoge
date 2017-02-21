be352ddb70a67a1fd6440e7a757653fd384225f1
k16125kk <so05dnoakaunnto@gmail.com>
2017-02-21 15:12:30 +0900

    tuck

---
+++ ensyuu0201.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+int main(void)
+{
+
+  int x,y;
+
+  puts("二つの整数を入力してください。");
+  printf("整数x:");   scanf("%d", &x);
+  printf("整数y:");   scanf("%d", &y);
+
+  printf("xの値はyの%d%%です。\n", x * 100 / y);
+
+  return 0;
+}
